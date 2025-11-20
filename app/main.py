9e2dc5883f9b5ec8a11a2322c10c0172978a408a
xander <xander@biltopia.org>
2025-11-20 19:35:51 +0100

    add pico css

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
 
 from app.api.routes import router
 from app.core.config import settings
@@ -13,5 +14,7 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(router)
